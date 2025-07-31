fbab3fb85b36f7969cd631cc51f36ae9650f6762
dane malloy <damalloy2@gmail.com>
2025-07-30 21:36:29 -0700

    working on optimizing types

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,23 @@
 export type Entity = number;
-export type Component = Record<string, unknown>;
-export type ComponentClass<T extends Component = Component> = new (...args: unknown[]) => T;
-export type ComponentQuest = ComponentClass[];
+
+export interface Component {
+	readonly __componentBrand?: never;
+}
+
+export type ComponentClass<T extends Component = Component> = new (...args: never[]) => T;
+export type ComponentQuery = readonly ComponentClass[];
+
+export type ComponentId = number;
+
+export interface Archetype {
+	readonly signature: ComponentId[];
+	readonly entities: Entity[];
+	readonly componentArrays: Map<ComponentId, Component[]>;
+}
+
+export interface QueryCache {
+	readonly signature: ComponentId[];
+	entities: Entity[];
+	archetypes: Set<Archetype>;
+	needsUpdate: boolean;
+}
