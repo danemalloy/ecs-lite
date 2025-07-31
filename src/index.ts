cc87f3d2526886e5fd665b740247585b950c4eb2
dane malloy <damalloy2@gmail.com>
2025-07-30 21:31:51 -0700

    updated index.ts for package creation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-export function makeHello(name: string) {
-	return `Hello from ${name}!`;
-}
+import { ComponentManager } from "./component";
+import { EntityManager } from "./entity";
+import { System, SystemManager } from "./system";
+import { Component, ComponentClass, ComponentQuest, Entity } from "./types";
+import { World } from "./world";
+
+export { Entity, Component, ComponentClass, ComponentQuest } from "./types";
+export { EntityManager } from "./entity";
+export { ComponentManager } from "./component";
+export { System, SystemManager } from "./system";
+export { World } from "./world";
+
+export default {
+	World,
+	Entity: undefined as unknown as Entity,
+	EntityManager,
+	Component: undefined as unknown as Component,
+	ComponentClass: undefined as unknown as ComponentClass,
+	ComponentQuest: undefined as unknown as ComponentQuest,
+	ComponentManager,
+	System: undefined as unknown as System,
+	SystemManager,
+};
